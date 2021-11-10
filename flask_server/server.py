78e0f182adaa58637b6294db96b4764d9ba135f5
cagedev <cagedev@gmail.com>
2021-11-10 08:19:56 +0100

    Added game_id. Github identity test.

--- flask_server/server.py
+++ flask_server/server.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/game/{game_id}")
+def game(game_id):
+    return f"<h1>Game {game_id}</h1>"
